94d51a7c870eb8aedf935369454e35634d46ca1c
roughness1 <mikhail200720@gmail.com>
2026-03-14 17:42:37 +0300

    Create functions.py
    функции из лабораторной

---
+++ functions.py
@@ -0,0 +1,51 @@
+def array_check(arr,target):
+    for i in arr:
+        if i==target:
+            return True
+    return False
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+def find_2nd_max(arr):
+    if len(arr) < 2:
+            return None  
+
+    if arr[0] > arr[1]:
+        max1 = arr[0]  
+        max2 = arr[1] 
+    else:
+        max1 = arr[1]
+        max2 = arr[0]
+    
+    for i in range(2, len(arr)):
+        if arr[i] > max1:
+            max2 = max1
+            max1 = arr[i]
+        elif arr[i] > max2 and arr[i] != max1:
+            max2 = arr[i]
+    
+    return max2
+
+def multiplication_table(n):
+    table = []
+    for i in range(1, n + 1):
+        row = []
+        for j in range(1, n + 1):
+            row.append(i * j)
+        table.append(row)
+    return table
